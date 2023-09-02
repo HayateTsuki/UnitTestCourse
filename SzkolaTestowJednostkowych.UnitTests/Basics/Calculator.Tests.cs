766a81547a03b86b9ab131459877b3b03b9e4326
Mateusz <krzeszowiecm1@gmail.com>
2023-09-02 18:43:15 +0200

    Add new test project proper to tested classes

---
+++ SzkolaTestowJednostkowych.UnitTests/Basics/Calculator.Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzkolaTestowJednostkowych.UnitTests.Basics
+{
+    class Calculator
+    {
+        public void Add_WhenCalled_ShouldReturnSum(){
+            
+        }
+    }
+}
